06b6b54cd8f34c3edaf97823b269d0f60e7c55ee
suppriya <suppriya@missionrnd.com>
2016-03-26 22:10:26 -0700

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -21,7 +21,103 @@ struct transaction {
 	char date[11];
 	char description[20];
 };
+int isolder1(char *dob1, char *dob2);
+struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) 
+{
+	if (((A != NULL) || (B != NULL)) && ((A != NULL) && (B != NULL)))
+	{
+		struct transaction *res = (struct transaction*)malloc(sizeof(struct transaction)*(ALen + BLen));
+		int r, i = 0, j = 0, l = 0;
+		while (i < ALen && j < BLen)
+		{
+			r = isolder1(A[i].date, B[j].date);
+			if (r == 0 || r == 1)
+			{
+				res[l] = A[i];
+				i++;
+			}
+			else
+			{
+				res[l] = B[j];
+				j++;
+			}
+			l++;
+		}
+		while (i < ALen)
+		{
+			res[l] = A[i];
+			i++;
+			l++;
+		}
+		while (j < BLen)
+		{
+			res[l] = B[j];
+			j++;
+			l++;
+		}
 
-struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+
+		return res;
+	}
+return NULL;
+}
+
+
+
+
+
+
+int isolder1(char *dob1, char *dob2)
+{
+	int i;
+	for (i = 6; i <= 9; i++)
+	{
+		if (dob1[i] == dob2[i])
+		{
+
+		}
+		else if (dob1[i] < dob2[i])
+		{
+			return 1;
+		}
+		else if (dob1[i] > dob2[i])
+		{
+			return 2;
+		}
+	}
+	for (i = 3; i < 5; i++)
+
+	{
+
+		if (dob1[i] == dob2[i])
+		{
+
+		}
+		else if (dob1[i] < dob2[i])
+		{
+			return 1;
+		}
+		else if (dob1[i] > dob2[i])
+		{
+			return 2;
+		}
+
+	}
+	for (i = 0; i < 2; i++)
+
+	{
+		if (dob1[i] == dob2[i])
+		{
+		}
+		else if (dob1[i] < dob2[i])
+		{
+			return 1;
+		}
+		else if (dob1[i] > dob2[i])
+		{
+			return 2;
+		}
+	}
+
+	return 0;
 }
